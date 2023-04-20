068d19e6caa15a6727724a7a048af0a941d8a9c9
Andrés Rentería <andres.renteria@puntored.co>
2023-04-20 12:55:14 -0500

    add google login button

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 import { AlertService } from 'src/app/services/alerts.service';
+
+declare const google: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
   public loginForm = this.fb.group({
     email: [
       localStorage.getItem('email') || '',
@@ -24,6 +26,29 @@ export class LoginComponent {
     private userService: UserService,
     private alertService: AlertService
   ) {}
+
+  ngAfterViewInit(): void {
+    this.googleInit();
+  }
+
+  @ViewChild('googleBtn') googleBtn: ElementRef | undefined;
+
+  googleInit() {
+    google.accounts.id.initialize({
+      client_id:
+        '903835936656-obikp14ppv2uj0pi442jop93ecscr9q5.apps.googleusercontent.com',
+      callback: this.handleCredentialResponse,
+    });
+    google.accounts.id.renderButton(
+      this.googleBtn?.nativeElement,
+      { theme: 'outline', size: 'large' } // customization attributes
+    );
+    google.accounts.id.prompt();
+  }
+
+  handleCredentialResponse(response: any) {
+    console.log('Encoded JWT ID token: ' + response.credential);
+  }
 
   public login() {
     this.userService.login(this.loginForm.value).subscribe(
