e68b15c55dc16bce36c4daf49a91d7b6a7b3dfd7
Andrés Rentería <andres.renteria@puntored.co>
2023-04-20 15:00:48 -0500

    add guard to pages

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -7,11 +7,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PagesComponent } from './pages.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: PagesComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: '', component: DashboardComponent, data: { title: 'Dashboard' } },
       {
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { tap, map, catchError } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
 
 import { RegisterForm } from '../interfaces/registerForm.interface';
 import { LoginForm } from '../interfaces/loginForm.interface';
+import { Observable, of } from 'rxjs';
 
 const base_url = `${environment.base_url}`;
 
@@ -37,5 +38,22 @@ export class UserService {
         localStorage.setItem('token', resp.token);
       })
     );
+  }
+
+  validateToken(): Observable<boolean> {
+    const localToken = localStorage.getItem('token') || '';
+    return this.http
+      .get(`${base_url}/login/renew`, {
+        headers: {
+          'x-token': localToken,
+        },
+      })
+      .pipe(
+        tap((resp: any) => {
+          localStorage.setItem('token', resp.token);
+        }),
+        map((resp) => true),
+        catchError((error) => of(false))
+      );
   }
 }
